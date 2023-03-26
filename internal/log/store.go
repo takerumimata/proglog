936cfa76f02ba54c93cfcff17e6d74b29e5509f0
Takeru Mimata <takeru.mimata.dev@gmail.com>
2023-03-26 15:36:35 +0900

    define log structure and impl append, read, readAt, close method for log

--- internal/log/store.go
+++ internal/log/store.go
@@ -20,12 +20,19 @@ const (
 )
 
 type store struct {
+	// ファイルへのポインタ
 	*os.File
-	mu   sync.Mutex
-	buf  *bufio.Writer
+	// mutex
+	mu sync.Mutex
+	// bufioのWriter
+	buf *bufio.Writer
+	// logのサイズ
 	size uint64
 }
 
+/*
+新規にlogを作成するためのmethod
+*/
 func newStore(f *os.File) (*store, error) {
 	fi, err := os.Stat(f.Name())
 	if err != nil {
@@ -37,4 +44,83 @@ func newStore(f *os.File) (*store, error) {
 		size: size,
 		buf:  bufio.NewWriter(f),
 	}, nil
+}
+
+/*
+ store構造体のメソッド。byte型でmessageを受け取り、保存する
+
+ return:
+  n uint64 レコードの長さ
+  pos uint64 positionの略。レコードの位置を返す
+  err error エラー
+*/
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	// Storeのlock
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// 書き込み前のサイズをpositionとして覚えておき、呼び出し元に返す。
+	pos = s.size
+	// レコードの長さをまず書く。レコードを読み出すときに何バイト読み出せばいいかわかるように。失敗したらerrorを返す
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	// 書き込みされたバイト数が返される
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	// header分を足し合わせて、レコードサイズとする
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+/*
+ store構造体のメソッド。positionを指定して、読み取りを行う
+*/
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	// レコードのheaderを読んで、レコードのサイズを取得する
+	size := make([]byte, lenWidth)
+	// 直前に取得したレコードのサイズと、メソッドの引数として渡されているポジションを渡してreadする。
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	// レコードサイズ分の容量を確保する
+	b := make([]byte, enc.Uint64(size))
+	// 確保したバッファ（b）に、読み込んだ内容を格納。
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+/*
+ store構造体のメソッド。
+*/
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+/*
+ ファイルをクローズする前にバッファされたデータを永続化する
+*/
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
 }
---
+++ internal/log/store_test.go
@@ -0,0 +1,123 @@
+package log
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+/*
+一時ファイルを作成し、Store構造体を作成する。
+Append、Read、ReadAtメソッドをそれぞれ呼び出して、エラーが起こらないことを確認する。
+Append、Read、ReadAtを呼び出すための関数はこのファイルでヘルパー関数として定義されている。
+*/
+func TestStoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+/*
+helper関数
+*/
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+/*
+helper関数
+*/
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+/*
+helper関数
+*/
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, lenWidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n)
+		off += int64(n)
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := os.CreateTemp("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterSize, _ := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+/*
+ヘルパー関数。ファイルへのポインタと、ファイルサイズを返す
+*/
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0600,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fi, err := f.Stat() // fiはfileInfoの略
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
